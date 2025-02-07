7c95e25f4441c1aa851476129e64416fa03ecef6
Fattah Muhyiddeen <fattahmuhyiddeen@gmail.com>
2025-02-07 10:27:13 +0800

    add post install

--- packages.js
+++ packages.js
@@ -1,20 +1,151 @@
-import * as React from "react";
-
-import * as Axios from "axios";
-import * as ReactRedux from "react-redux";
-import * as LottieReactNative from "lottie-react-native";
-import * as ReactNative from "react-native";
-import * as ReactNativeSafeAreaContext from "react-native-safe-area-context";
-import * as ReactNativeSvg from "react-native-svg";
+import * as ADOBEREACTNATIVEAEPASSURANCEx0 from "@adobe/react-native-aepassurance"
+import * as ADOBEREACTNATIVEAEPCOREx1 from "@adobe/react-native-aepcore"
+import * as ADOBEREACTNATIVEAEPEDGEx2 from "@adobe/react-native-aepedge"
+import * as ADOBEREACTNATIVEAEPTARGETx3 from "@adobe/react-native-aeptarget"
+import * as DYNATRACEREACTNATIVEPLUGINx4 from "@dynatrace/react-native-plugin"
+import * as LEANPLUMREACTNATIVESDKx5 from "@leanplum/react-native-sdk"
+import * as MREACTNATIVEESIMACTIVATIONx6 from "@m1/react-native-esim-activation"
+import * as MICROSOFTAPPLICATIONINSIGHTSREACTNATIVEx7 from "@microsoft/applicationinsights-react-native"
+import * as MICROSOFTAPPLICATIONINSIGHTSWEBx8 from "@microsoft/applicationinsights-web"
+import * as REACTNATIVEASYNCSTORAGEASYNCSTORAGEx9 from "@react-native-async-storage/async-storage"
+import * as REACTNATIVECLIPBOARDCLIPBOARDx10 from "@react-native-clipboard/clipboard"
+import * as REACTNATIVECOMMUNITYNETINFOx11 from "@react-native-community/netinfo"
+import * as REACTNATIVECOMMUNITYPUSHNOTIFICATIONIOSx12 from "@react-native-community/push-notification-ios"
+import * as REACTNAVIGATIONBOTTOMTABSx13 from "@react-navigation/bottom-tabs"
+import * as REACTNAVIGATIONNATIVEx14 from "@react-navigation/native"
+import * as REACTNAVIGATIONNATIVESTACKx15 from "@react-navigation/native-stack"
+import * as REDUXJSTOOLKITx16 from "@reduxjs/toolkit"
+import * as APPCENTERx17 from "appcenter"
+import * as APPCENTERANALYTICSx18 from "appcenter-analytics"
+import * as APPCENTERCRASHESx19 from "appcenter-crashes"
+import * as AXIOSx20 from "axios"
+import * as CLEVERTAPREACTNATIVEx21 from "clevertap-react-native"
+import * as EMOJIFLAGSx22 from "emoji-flags"
+import * as FORMIKx23 from "formik"
+import * as IMMUTABLEx24 from "immutable"
+import * as JWTDECODEx25 from "jwt-decode"
+import * as LODASHx26 from "lodash"
+import * as LOTTIEIOSx27 from "lottie-ios"
+import * as LOTTIEREACTNATIVEx28 from "lottie-react-native"
+import * as MOMENTx29 from "moment"
+import * as MOMENTTIMEZONEx30 from "moment-timezone"
+import * as QUERYSTRINGx31 from "query-string"
+import * as REACTx32 from "react"
+import * as REACTNATIVEx33 from "react-native"
+import * as REACTNATIVEBLOBUTILx34 from "react-native-blob-util"
+import * as REACTNATIVECALENDAREVENTSx35 from "react-native-calendar-events"
+import * as REACTNATIVECALENDARSx36 from "react-native-calendars"
+import * as REACTNATIVECHECKBOXx37 from "react-native-check-box"
+import * as REACTNATIVECLICKOUTSIDEx38 from "react-native-click-outside"
+import * as REACTNATIVECONTACTSx39 from "react-native-contacts"
+import * as REACTNATIVECOUNTDOWNCOMPONENTx40 from "react-native-countdown-component"
+import * as REACTNATIVEDASHx41 from "react-native-dash"
+import * as REACTNATIVEDEVICEINFOx42 from "react-native-device-info"
+import * as REACTNATIVEGESTUREHANDLERx43 from "react-native-gesture-handler"
+import * as REACTNATIVEGETRANDOMVALUESx44 from "react-native-get-random-values"
+import * as REACTNATIVEHTMLVIEWx45 from "react-native-htmlview"
+import * as REACTNATIVEINAPPDEBUGGERx46 from "react-native-in-app-debugger"
+import * as REACTNATIVEINAPPBROWSERREBORNx47 from "react-native-inappbrowser-reborn"
+import * as REACTNATIVEKEYCHAINx48 from "react-native-keychain"
+import * as REACTNATIVEMODALx49 from "react-native-modal"
+import * as REACTNATIVEMULTILAYERDONUTCHARTx50 from "react-native-multi-layer-donut-chart"
+import * as REACTNATIVEPDFx51 from "react-native-pdf"
+import * as REACTNATIVEPERMISSIONSx52 from "react-native-permissions"
+import * as REACTNATIVEQRCODESVGx53 from "react-native-qrcode-svg"
+import * as REACTNATIVERATEx54 from "react-native-rate"
+import * as REACTNATIVERENDERHTMLx55 from "react-native-render-html"
+import * as REACTNATIVESAFEAREACONTEXTx56 from "react-native-safe-area-context"
+import * as REACTNATIVESCREENSx57 from "react-native-screens"
+import * as REACTNATIVESHAx58 from "react-native-sha256"
+import * as REACTNATIVESHAREx59 from "react-native-share"
+import * as REACTNATIVESNAPCAROUSELx60 from "react-native-snap-carousel"
+import * as REACTNATIVESOUNDx61 from "react-native-sound"
+import * as REACTNATIVESVGx62 from "react-native-svg"
+import * as REACTNATIVESWIPERx63 from "react-native-swiper"
+import * as REACTNATIVEURLPOLYFILLx64 from "react-native-url-polyfill"
+import * as REACTNATIVEWALKTHROUGHTOOLTIPx65 from "react-native-walkthrough-tooltip"
+import * as REACTNATIVEWEBVIEWx66 from "react-native-webview"
+import * as REACTREDUXx67 from "react-redux"
+import * as REDUXPERSISTx68 from "redux-persist"
+import * as RNBIOMETRICAUTHENTICATIONx69 from "rn-biometric-authentication"
+import * as TEALIUMREACTNATIVEx70 from "tealium-react-native"
+import * as UUIDx71 from "uuid"
+import * as YUPx72 from "yup"
 const Packages = {
-  axios: () => Axios,
-  react: () => React,
-  "react-native": () => ReactNative,
-  "react-redux": () => ReactRedux,
-  "react-native-svg": () => ReactNativeSvg,
-  "react-native-safe-area-context": () => ReactNativeSafeAreaContext,
-  "lottie-react-native": () => LottieReactNative,
-};
+   "@adobe/react-native-aepassurance": () => ADOBEREACTNATIVEAEPASSURANCEx0,
+   "@adobe/react-native-aepcore": () => ADOBEREACTNATIVEAEPCOREx1,
+   "@adobe/react-native-aepedge": () => ADOBEREACTNATIVEAEPEDGEx2,
+   "@adobe/react-native-aeptarget": () => ADOBEREACTNATIVEAEPTARGETx3,
+   "@dynatrace/react-native-plugin": () => DYNATRACEREACTNATIVEPLUGINx4,
+   "@leanplum/react-native-sdk": () => LEANPLUMREACTNATIVESDKx5,
+   "@m1/react-native-esim-activation": () => MREACTNATIVEESIMACTIVATIONx6,
+   "@microsoft/applicationinsights-react-native": () => MICROSOFTAPPLICATIONINSIGHTSREACTNATIVEx7,
+   "@microsoft/applicationinsights-web": () => MICROSOFTAPPLICATIONINSIGHTSWEBx8,
+   "@react-native-async-storage/async-storage": () => REACTNATIVEASYNCSTORAGEASYNCSTORAGEx9,
+   "@react-native-clipboard/clipboard": () => REACTNATIVECLIPBOARDCLIPBOARDx10,
+   "@react-native-community/netinfo": () => REACTNATIVECOMMUNITYNETINFOx11,
+   "@react-native-community/push-notification-ios": () => REACTNATIVECOMMUNITYPUSHNOTIFICATIONIOSx12,
+   "@react-navigation/bottom-tabs": () => REACTNAVIGATIONBOTTOMTABSx13,
+   "@react-navigation/native": () => REACTNAVIGATIONNATIVEx14,
+   "@react-navigation/native-stack": () => REACTNAVIGATIONNATIVESTACKx15,
+   "@reduxjs/toolkit": () => REDUXJSTOOLKITx16,
+   "appcenter": () => APPCENTERx17,
+   "appcenter-analytics": () => APPCENTERANALYTICSx18,
+   "appcenter-crashes": () => APPCENTERCRASHESx19,
+   "axios": () => AXIOSx20,
+   "clevertap-react-native": () => CLEVERTAPREACTNATIVEx21,
+   "emoji-flags": () => EMOJIFLAGSx22,
+   "formik": () => FORMIKx23,
+   "immutable": () => IMMUTABLEx24,
+   "jwt-decode": () => JWTDECODEx25,
+   "lodash": () => LODASHx26,
+   "lottie-ios": () => LOTTIEIOSx27,
+   "lottie-react-native": () => LOTTIEREACTNATIVEx28,
+   "moment": () => MOMENTx29,
+   "moment-timezone": () => MOMENTTIMEZONEx30,
+   "query-string": () => QUERYSTRINGx31,
+   "react": () => REACTx32,
+   "react-native": () => REACTNATIVEx33,
+   "react-native-blob-util": () => REACTNATIVEBLOBUTILx34,
+   "react-native-calendar-events": () => REACTNATIVECALENDAREVENTSx35,
+   "react-native-calendars": () => REACTNATIVECALENDARSx36,
+   "react-native-check-box": () => REACTNATIVECHECKBOXx37,
+   "react-native-click-outside": () => REACTNATIVECLICKOUTSIDEx38,
+   "react-native-contacts": () => REACTNATIVECONTACTSx39,
+   "react-native-countdown-component": () => REACTNATIVECOUNTDOWNCOMPONENTx40,
+   "react-native-dash": () => REACTNATIVEDASHx41,
+   "react-native-device-info": () => REACTNATIVEDEVICEINFOx42,
+   "react-native-gesture-handler": () => REACTNATIVEGESTUREHANDLERx43,
+   "react-native-get-random-values": () => REACTNATIVEGETRANDOMVALUESx44,
+   "react-native-htmlview": () => REACTNATIVEHTMLVIEWx45,
+   "react-native-in-app-debugger": () => REACTNATIVEINAPPDEBUGGERx46,
+   "react-native-inappbrowser-reborn": () => REACTNATIVEINAPPBROWSERREBORNx47,
+   "react-native-keychain": () => REACTNATIVEKEYCHAINx48,
+   "react-native-modal": () => REACTNATIVEMODALx49,
+   "react-native-multi-layer-donut-chart": () => REACTNATIVEMULTILAYERDONUTCHARTx50,
+   "react-native-pdf": () => REACTNATIVEPDFx51,
+   "react-native-permissions": () => REACTNATIVEPERMISSIONSx52,
+   "react-native-qrcode-svg": () => REACTNATIVEQRCODESVGx53,
+   "react-native-rate": () => REACTNATIVERATEx54,
+   "react-native-render-html": () => REACTNATIVERENDERHTMLx55,
+   "react-native-safe-area-context": () => REACTNATIVESAFEAREACONTEXTx56,
+   "react-native-screens": () => REACTNATIVESCREENSx57,
+   "react-native-sha256": () => REACTNATIVESHAx58,
+   "react-native-share": () => REACTNATIVESHAREx59,
+   "react-native-snap-carousel": () => REACTNATIVESNAPCAROUSELx60,
+   "react-native-sound": () => REACTNATIVESOUNDx61,
+   "react-native-svg": () => REACTNATIVESVGx62,
+   "react-native-swiper": () => REACTNATIVESWIPERx63,
+   "react-native-url-polyfill": () => REACTNATIVEURLPOLYFILLx64,
+   "react-native-walkthrough-tooltip": () => REACTNATIVEWALKTHROUGHTOOLTIPx65,
+   "react-native-webview": () => REACTNATIVEWEBVIEWx66,
+   "react-redux": () => REACTREDUXx67,
+   "redux-persist": () => REDUXPERSISTx68,
+   "rn-biometric-authentication": () => RNBIOMETRICAUTHENTICATIONx69,
+   "tealium-react-native": () => TEALIUMREACTNATIVEx70,
+   "uuid": () => UUIDx71,
+   "yup": () => YUPx72,
+}
 
 const fromPairs = (pairs) =>
   Object.assign({}, ...pairs.map(([k, v]) => ({ [k]: v })));
---
+++ postinstall.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const packageFile = require('../../package.json');
+
+const packagesFile = fs.openSync('./packages.js', 'w');
+
+const libs = Object.keys(packageFile.dependencies);
+
+const varName = (key, index) => key.replace(/[^A-Za-z]/g, '').toUpperCase() + 'x' + index;
+
+for (let i = 0, l = libs.length; i < l; i++) {
+  fs.writeSync(packagesFile, `import * as ${varName(libs[i], i)} from "${libs[i]}"\n`);
+}
+
+fs.writeSync(packagesFile, 'const Packages = {\n');
+for (let i = 0, l = libs.length; i < l; i++) {
+    fs.writeSync(packagesFile, `   "${libs[i]}": () => ${varName(libs[i], i)},\n`);
+  }
+fs.writeSync(packagesFile, '};\n');
+fs.writeSync(packagesFile, `
+const fromPairs = (pairs) =>
+  Object.assign({}, ...pairs.map(([k, v]) => ({ [k]: v })));
+export default fromPairs(
+  Object.keys(Packages).map((k) => [k, () => ({ exports: Packages[k]() })])
+);`);
+
+fs.closeSync(packagesFile);
